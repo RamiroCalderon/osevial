021f77925541aa977b1cfffbe7a475be4fdb0dbb
Ramiro Calderon <ramirocalderon@oxschool.edu.mx>
2021-07-01 12:57:39 -0500

    add new campus

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,4 +1,29 @@
 class SchoolsController < ApplicationController
   def index
+   @schools = School.all
   end
+
+  def show
+    @school = School.find(params[:id])
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.new(school_params)
+
+    if @school.save
+      redirect_to @school
+    else
+      render :new
+    end
+  end
+
+  private
+  def school_params
+    params.require(:school).permit(:name)
+  end
+
 end
